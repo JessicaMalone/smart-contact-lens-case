6a25d049c52467e83d308d6ff6c39e40573b6894
Peter Ma <nyceane@gmail.com>
2020-01-03 17:13:12 -0800

    added alexa server code

---
+++ skill-nodejs/lambda/index.js
@@ -0,0 +1,228 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the terms and conditions 
+ * set forth in the accompanying LICENSE.TXT file.
+ *
+ * THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY DISCLAIMS, WITH 
+ * RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+*/
+
+// This skill sample demonstrates how to send directives and receive events from an Echo connected gadget.
+// This skill uses the Alexa Skills Kit SDK (v2). Please visit https://alexa.design/cookbook for additional
+// examples on implementing slots, dialog management, session persistence, api calls, and more.
+
+const Alexa = require('ask-sdk-core');
+const Util = require('./util');
+const Common = require('./common');
+
+// The audio tag to include background music
+//const BG_MUSIC = '<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_waiting_loop_30s_01"/>';
+
+// The namespace of the custom directive to be sent by this skill
+const NAMESPACE = 'Custom.SmartCaseGadget';
+
+// The name of the custom directive to be sent this skill
+const NAME_CONTROL = 'control';
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle: async function(handlerInput) {
+
+        const request = handlerInput.requestEnvelope;
+        const { apiEndpoint, apiAccessToken } = request.context.System;
+        const apiResponse = await Util.getConnectedEndpoints(apiEndpoint, apiAccessToken);
+        if ((apiResponse.endpoints || []).length === 0) {
+            return handlerInput.responseBuilder
+            .speak(`I couldn't find an Smart Lens Case connected to this Echo device. Please check to make sure your Smart Lens Case is connected, and try again.`)
+            .getResponse();
+        }
+
+        // Store the gadget endpointId to be used in this skill session
+        const endpointId = apiResponse.endpoints[0].endpointId || [];
+        Util.putSessionAttribute(handlerInput, 'endpointId', endpointId);
+
+        // Set skill duration to 5 minutes (ten 30-seconds interval)
+        Util.putSessionAttribute(handlerInput, 'duration', 10);
+
+        // Set the token to track the event handler
+        const token = handlerInput.requestEnvelope.request.requestId;
+        Util.putSessionAttribute(handlerInput, 'token', token);
+
+        let speechOutput = "Welcome to Smart Lens Case";
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .addDirective(Util.buildStartEventHandler(token,60000, {}))
+            .getResponse();
+    }
+};
+
+const EventsReceivedRequestHandler = {
+    // Checks for a valid token and endpoint.
+    canHandle(handlerInput) {
+        let { request } = handlerInput.requestEnvelope;
+        console.log('Request type: ' + Alexa.getRequestType(handlerInput.requestEnvelope));
+        if (request.type !== 'CustomInterfaceController.EventsReceived') return false;
+
+        const attributesManager = handlerInput.attributesManager;
+        let sessionAttributes = attributesManager.getSessionAttributes();
+        let customEvent = request.events[0];
+
+        // Validate event token
+        if (sessionAttributes.token !== request.token) {
+            console.log("Event token doesn't match. Ignoring this event");
+            return false;
+        }
+
+        // Validate endpoint
+        let requestEndpoint = customEvent.endpoint.endpointId;
+        if (requestEndpoint !== sessionAttributes.endpointId) {
+            console.log("Event endpoint id doesn't match. Ignoring this event");
+            return false;
+        }
+        return true;
+    },
+    handle(handlerInput) {
+
+        return handlerInput.responseBuilder
+            .speak("Found button has been pressed.", "REPLACE_ALL")
+            .getResponse();
+    }
+};
+const ExpiredRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'CustomInterfaceController.Expired'
+    },
+    handle(handlerInput) {
+        console.log("== Custom Event Expiration Input ==");
+
+        // Set the token to track the event handler
+        const token = handlerInput.requestEnvelope.request.requestId;
+        Util.putSessionAttribute(handlerInput, 'token', token);
+
+        const attributesManager = handlerInput.attributesManager;
+        let duration = attributesManager.getSessionAttributes().duration || 0;
+        if (duration > 0) {
+            Util.putSessionAttribute(handlerInput, 'duration', --duration);
+
+            // Extends skill session
+            const speechOutput = `${duration} minutes remaining.`;
+            return handlerInput.responseBuilder
+                .addDirective(Util.buildStartEventHandler(token, 60000, {}))
+                .speak(speechOutput)
+                .getResponse();
+        }
+        else {
+            // End skill session
+            return handlerInput.responseBuilder
+                .speak("Skill duration expired. Goodbye.")
+                .withShouldEndSession(true)
+                .getResponse();
+        }
+    }
+};
+
+
+// Construct and send a custom directive to the connected gadget with data from
+// the LightIntentHandler.
+const LightIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'LightIntent';
+    },
+    handle: function (handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        let endpointId = attributesManager.getSessionAttributes().endpointId || [];
+
+        // Construct the directive with the payload containing the move parameters
+        let directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'light'
+            });
+
+        let speechOutput = 'blinking led on your smart lens case';
+        
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .addDirective(directive)
+            .getResponse();
+    }
+};
+
+// Construct and send a custom directive to the connected gadget with data from
+// the LightIntentHandler.
+const WhereIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'WhereIntent';
+    },
+    handle: function (handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        let endpointId = attributesManager.getSessionAttributes().endpointId || [];
+
+        // Construct the directive with the payload containing the move parameters
+        let directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'where'
+            });
+
+        let speechOutput = 'Finding your smart lens case';
+        
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .addDirective(directive)
+            .getResponse();
+    }
+};
+
+// Construct and send a custom directive to the connected gadget with data from
+// the LightIntentHandler.
+const SoundIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SoundIntent';
+    },
+    handle: function (handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        let endpointId = attributesManager.getSessionAttributes().endpointId || [];
+
+        // Construct the directive with the payload containing the move parameters
+        let directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'sound'
+            });
+
+        let speechOutput = 'Beeping your smart lens case';
+        
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .addDirective(directive)
+            .getResponse();
+    }
+};
+    
+    
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        LightIntentHandler,
+        WhereIntentHandler,
+        SoundIntentHandler,
+        EventsReceivedRequestHandler,
+        ExpiredRequestHandler,
+        Common.HelpIntentHandler,
+        Common.CancelAndStopIntentHandler,
+        Common.SessionEndedRequestHandler,
+        Common.IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addRequestInterceptors(Common.RequestInterceptor)
+    .addErrorHandlers(
+        Common.ErrorHandler,
+    )
+    .lambda();
